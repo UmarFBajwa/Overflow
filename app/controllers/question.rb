f2a60a1e845e4122fc5e82e4245d2073984cfa69
Robin Soubry & Brian Mosley & David <pair+RobinSoubry.brianlmosley.davidthegreat@devbootcamp.com>
2016-06-03 11:39:50 -0700

    Update list of questions to include id in questions hash

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,6 +1,6 @@
 get '/questions' do
   questions = Question.all
-  @questions_h = questions.map {|q| {title: q.title, votes: q.votes.count, answers: q.answers.count, date:q.created_at}}
+  @questions_h = questions.map {|q| {id: q.id, title: q.title, votes: q.votes.count, answers: q.answers.count, date:q.created_at}}
   erb :'questions/index'
 end
 
